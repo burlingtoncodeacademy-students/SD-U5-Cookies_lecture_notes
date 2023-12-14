004f1729467ff2a4a878839bbf186eec0ae31089
Eric Winebrenner <ejwinebrenner@gmail.com>
2023-12-14 13:56:44 -0500

    updated info

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
-const db = require('./db.json');
+const PORT = 4000; // Server is running on Port Number 4000
+const db = require('./db.json'); // references the db.json content
 
+//* Middleware
+
+
+//* Routes
 app.post('/', async(req,res) => {
 
     [
@@ -10,15 +14,13 @@ app.post('/', async(req,res) => {
         {"item":'cheese', 'price':2.99},
         {"item":'dog food', 'price':8.98},
     ]
+    // Mock shopping cart User has found elsewhere
 
     res.send(`Cookies have been saved.`)
 });
 
 app.get('/', (req,res) => {
 
-    let offer;
-
-    
 })
 
 app.listen(PORT, () => console.log(`Running: ${PORT}`));
